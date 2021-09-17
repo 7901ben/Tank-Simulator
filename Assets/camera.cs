d0b259358b1e801d93483c575a39712a1b34c2b7
Benin B Sam <benin7895123@gmail.com>
2021-09-17 11:53:49 +0530

    tank sim

---
+++ Assets/camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public Transform player;
+    //vector3 stores 3 floats ie positions(x,y,z)
+    public Vector3 offset;  //we specifed offset values in unity console in camera 
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(player.position);  
+        transform.position = player.position + offset;  //if we write transform with nin capital t,it reffers to transform of current object that means the object on which the script is sitting on ie here its camera because there is where the scrip is
+                                                        //in the above code the camera is inside the cube this can be useful for 1st person view but we want only 3rd person view
+
+    }
+}
+
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(1, 3, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("f"))
+        {
+            rb.AddForce(0, 500 * Time.deltaTime, 0);
+           
+        }
+        if (Input.GetKey("l"))
+        {
+            rb.AddForce(0, 2000 * Time.deltaTime, 0);
+        }
+    }
+}
